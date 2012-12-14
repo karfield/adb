1ceec66783c6a3707b6a5aee92650e24745b54fd
Karfield Chen <karfield@126.com>
2012-12-14 12:07:35 +0800

    modify gen_udev_rules.sh
    Signed-off-by: Karfield Chen <karfield@126.com>

--- debian/gen_udev_rules.sh
+++ debian/gen_udev_rules.sh
@@ -30,10 +30,15 @@ while test $# -ne 0; do
 done
 
 echo "# Android USB Devices" > $rules_file
-vid_list=$(cat $usb_vendor_ids_c | grep "0x" | sed -n "s/0x\([^,]*\).*/\1/p")
-for vid in $vid_list; do
-    echo "SUBSYSTEM==\"usb\", SYSFS{idVendor}==\"$vid\", MODE=\"0666\"," \
-        "GROUP=\"plugdev\"" >> $rules_file
+code_lines=$(cat $usb_vendor_ids_c | grep "0x")
+IFS=$(echo -en "\n\r")
+for l in $code_lines; do
+	vid=$(echo $l|sed -n 's/.*0x\([^,]*\).*/\1/p')
+	vendor=$(echo $l|sed -n 's/.*\/\* *\(.*\)\*\//\1/p')
+    cat >> $rules_file << EOL
+# $vendor
+SUBSYSTEM=="usb", SYSFS{idVendor}=="$vid", MODE="0666", GROUP="plugdev"
+EOL
 done
 
 chmod $mode $rules_file
